5cf388923f309d654291dcf1fe6a97e9913ce0cf
Geordon Worley <vadixidav@gmail.com>
2021-06-21 18:03:31 -0500

    add greedy search and basic implementation details

--- src/lib.rs
+++ src/lib.rs
@@ -10,7 +10,7 @@ pub struct Gnn<K, V> {
     entries: Vec<(K, V)>,
 }
 
-pub struct GnnNeighbor<K> {
+struct GnnNeighbor<K> {
     key: K,
     node: usize,
 }
@@ -32,8 +32,41 @@ impl<K, V> Gnn<K, V>
 where
     K: MetricPoint,
 {
-    fn insert(&mut self, key: K, value: V) {
-        unimplemented!()
+    fn insert(&mut self, key: K, value: V) -> usize {
+        // First we perform a greedy search to find the greedy destination.
+        if let Some(greedy_nearest) = self.greedy_search(&key) {
+            // Once we have the nearest neighbor from the greedy search,
+            // we need to find the real nearest neighbor.
+            unimplemented!()
+        } else {
+            // In this case, there is nothing in the GNN, so add the item and return.
+            self.nodes.push(GnnNode { neighbors: vec![] });
+            self.entries.push((key, value));
+            0
+        }
+    }
+
+    fn greedy_search(&self, query_key: &K) -> Option<usize> {
+        if self.entries.is_empty() {
+            None
+        } else {
+            let mut to_search = vec![0];
+            let mut best_distance = query_key.distance(&self.entries[0].0);
+            let mut best_index = 0;
+
+            while let Some(search_node) = to_search.pop() {
+                for GnnNeighbor { key, node } in &self.nodes[search_node].neighbors {
+                    let distance = query_key.distance(key);
+                    if distance < best_distance {
+                        to_search.push(*node);
+                        best_index = *node;
+                        best_distance = distance;
+                    }
+                }
+            }
+
+            Some(best_index)
+        }
     }
 }
 

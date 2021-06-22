b05b9e7cdb2b17a177ab3f1793f75d8fbefdea5b
Geordon Worley <vadixidav@gmail.com>
2021-06-21 19:04:10 -0500

    finish the basic insert logic

--- src/lib.rs
+++ src/lib.rs
@@ -30,18 +30,39 @@ impl<K, V> Gnn<K, V> {
 
 impl<K, V> Gnn<K, V>
 where
-    K: MetricPoint,
+    K: MetricPoint + Clone,
 {
-    fn insert(&mut self, key: K, value: V) -> usize {
+    fn insert(&mut self, insert_key: K, insert_value: V) -> usize {
         // First we perform a greedy search to find the greedy destination.
-        if let Some(greedy_nearest) = self.greedy_search(&key) {
+        if let Some(greedy_nn) = self.greedy_search(&insert_key) {
             // Once we have the nearest neighbor from the greedy search,
             // we need to find the real nearest neighbor.
-            unimplemented!()
+            let real_nn = self
+                .entries
+                .iter()
+                .enumerate()
+                .min_by_key(|(_, (key, value))| insert_key.distance(key))
+                .unwrap()
+                .0;
+
+            // We need to point the greedy nearest neighbor, which is along the greedy search path,
+            // to the real nearest neighbor. This allows a greedy search path to the target which also intersects
+            // with the nearest neighbor.
+            self.add_neighbor(greedy_nn, real_nn);
+
+            // Insert the node itself.
+            let insert_node = self.entries.len();
+            self.nodes.push(GnnNode { neighbors: vec![] });
+            self.entries.push((insert_key, insert_value));
+
+            // Connect the node to its nearest neighbor.
+            self.add_neighbor(real_nn, insert_node);
+
+            insert_node
         } else {
             // In this case, there is nothing in the GNN, so add the item and return.
             self.nodes.push(GnnNode { neighbors: vec![] });
-            self.entries.push((key, value));
+            self.entries.push((insert_key, insert_value));
             0
         }
     }
@@ -67,6 +88,25 @@ where
 
             Some(best_index)
         }
+    }
+
+    /// Adds an edge to the DAG. This will not add the edge if it already exists.
+    pub fn add_neighbor(&mut self, source: usize, target: usize) {
+        // First, check if the neighbor already exists.
+        if self.nodes[source]
+            .neighbors
+            .iter()
+            .any(|neighbor| neighbor.node == target)
+        {
+            // If it does, don't add it.
+            return;
+        }
+        let neighbor = GnnNeighbor {
+            key: self.entries[target].0.clone(),
+            node: target,
+        };
+
+        self.nodes[source].neighbors.push(neighbor);
     }
 }
 
